1a2c7d6b61160ac6c8014c47d3e886cd80d8e451
Tim <tim.fink519@gmail.com>
2022-08-10 18:24:50 -0400

    commit tree 9040f644f815bcf34ce5759d8d5b0d05506670a9

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName.toUpperCase();
+}
+    
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+    return bestCustomer;
+} 
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 4
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 7;
+    return leastFavoriteCustomer;
+}
